754fcc768cf02bd209f25a6aebfbbbb56601b380
anujsb <141896390+anujsb@users.noreply.github.com>
2025-01-20 19:33:31 +0530

    clerk auth setup complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import UserMenu from "./user-menu";
+// import { checkUser } from "@/lib/checkUser";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { PenBox } from "lucide-react";
 
-const Header = () => {
+async function Header() {
+  // await checkUser();
+
   return (
-    <div>Header</div>
-  )
+    <nav className="mx-auto py-2 px-4 flex justify-between items-center shadow-md border-b-2">
+      <Link href="/" className="flex items-center">
+        <Image
+          src="/favicon.ico"
+          width="150"
+          height="60"
+          alt="Schedulrr Logo"
+          className="h-16 w-auto"
+        />
+      </Link>
+
+      <div className="flex items-center gap-4">
+        <Link href="/events?create=true">
+          <Button variant="default" className="flex items-center gap-2">
+            <PenBox size={18} />
+            <span className="hidden sm:inline">Create Event</span>
+          </Button>
+        </Link>
+        <SignedOut>
+          <SignInButton forceRedirectUrl="/dashboard">
+            <Button variant="outline">Login</Button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserMenu />
+          <UserButton />
+        </SignedIn>
+      </div>
+    </nav>
+  );
 }
 
-export default Header
+export default Header;
